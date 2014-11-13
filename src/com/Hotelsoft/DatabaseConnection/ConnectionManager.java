4fcb9aca2de000bbefa64b6d251d336df08d2954
Anish <anishsk@users.noreply.github.com>
2014-11-13 17:56:03 +0530

    Commented Unused imports.

--- src/com/Hotelsoft/DatabaseConnection/ConnectionManager.java
+++ src/com/Hotelsoft/DatabaseConnection/ConnectionManager.java
@@ -1,45 +1,44 @@
 package com.Hotelsoft.DatabaseConnection;
 
-import java.io.FileInputStream;
-import java.io.InputStream;
+/*import java.io.FileInputStream;
+import java.io.InputStream;*/
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.util.ArrayList;
+/*import java.util.ArrayList;
 import java.util.HashMap;
-import java.util.Properties;
+import java.util.Properties;*/
 
 import com.mysql.jdbc.Connection;
-import com.mysql.jdbc.ResultSet;
-import com.mysql.jdbc.Statement;
+/*import com.mysql.jdbc.ResultSet;
+import com.mysql.jdbc.Statement;*/
 
 public class ConnectionManager {
-	
-	
-  public Connection connection = null;
-	 static String dbuser = null;
-	 static String dbpassword = null;
-	 
-	 
-	 public Connection getConnection ()
-	 {
-		 
-		 //String query = "select * from menuitem";
-		 try {
+
+	public Connection connection = null;
+	static String dbuser = null;
+	static String dbpassword = null;
+
+	public Connection getConnection() {
+		/*
+		 * Function to create connection to database 
+		 */
+		try {
 			Class.forName("com.mysql.jdbc.Driver").newInstance();
 		} catch (InstantiationException | IllegalAccessException
 				| ClassNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-			try {
-				connection = (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/alive", "root","");
+		try {
+			connection = (Connection) DriverManager.getConnection(
+					"jdbc:mysql://127.0.0.1:3306/alive", "root", "anishkumar");
 
-			} catch (SQLException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			}
-			System.out.println("Connection Successful");
-	
-			return connection;
-	 }
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		System.out.println("Connection Successful");
+
+		return connection;
+	}
 }
