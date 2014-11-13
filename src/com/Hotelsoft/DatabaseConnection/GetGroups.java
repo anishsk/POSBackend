9ce3cbe23c9937901d94aa1ffbea08c1d4b1e8ac
Anish <anishsk@users.noreply.github.com>
2014-11-13 22:12:22 +0530

    added finally clause to try catch inside returnGroupList() to close connections after everything is done. moved hashmap declarations inside the returnGroupList(). Use of ResultSetImpl instead of ResultSet from MySql

--- src/com/Hotelsoft/DatabaseConnection/GetGroups.java
+++ src/com/Hotelsoft/DatabaseConnection/GetGroups.java
@@ -4,12 +4,14 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
 
-import com.mysql.jdbc.ResultSet;
+import com.mysql.jdbc.ResultSetImpl;
 import com.mysql.jdbc.Statement;
+
+//import com.mysql.jdbc.ResultSet;//use only with old version of MySQL
 
 public class GetGroups extends ConnectionManager {
 	private Statement st = null;
-	private ResultSet rs = null, temprs = null;
+	private ResultSetImpl rs = null;
 
 	public GetGroups() {
 		System.out.println("In Get Groups Constructor. getting Connection");
@@ -17,7 +19,7 @@ public class GetGroups extends ConnectionManager {
 
 		try {
 			st = (Statement) connection.createStatement();
-			rs = (ResultSet) st
+			rs = (ResultSetImpl) st
 					.executeQuery("SELECT groups.deptno, groups.groupname,groups.groupno from groups");
 			System.out.println("in cins");
 
@@ -26,37 +28,46 @@ public class GetGroups extends ConnectionManager {
 		}
 	}
 
-	public ArrayList returnGroupList(){
+	@SuppressWarnings({ "rawtypes", "unchecked" })
+	public ArrayList returnGroupList() {
 		ArrayList groupList = new ArrayList();
-		//HashMap tempGroupHold = new HashMap();
-	//	HashMap copyGroup = new HashMap();
-		String cmp =null;
+		// HashMap tempGroupHold = new HashMap();
+		// HashMap copyGroup = new HashMap();
+		// String cmp =null;
 		String groupName = null;
 		String groupNo = null;
 		String groupDeptNo = null;
-		
+
 		try {
-			while(rs.next()){
+			while (rs.next()) {
 				groupDeptNo = rs.getString("deptno");
 				groupNo = rs.getString("groupno");
 				groupName = rs.getString("groupname");
 				HashMap copyGroup = new HashMap();
-			
 				HashMap tempGroupHold = new HashMap();
-			
-						//tempGroupHold.clear();
-						tempGroupHold.put("deptNo", groupDeptNo);
-						tempGroupHold.put("groupName", groupName);
-						tempGroupHold.put("groupNo", groupNo);
-						copyGroup=(HashMap) tempGroupHold.clone();
-					
-					groupList.add(tempGroupHold);
-					cmp = groupName;
-				}
-				System.out.println(groupList);
+
+				// tempGroupHold.clear();
+				tempGroupHold.put("deptNo", groupDeptNo);
+				tempGroupHold.put("groupName", groupName);
+				tempGroupHold.put("groupNo", groupNo);
+				copyGroup = (HashMap) tempGroupHold.clone();
+
+				groupList.add(copyGroup);
+				// cmp = groupName;
+			}
+			System.out.println(groupList);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			try {
+				st.close();
+				rs.close();
+				connection.close();
 			} catch (SQLException e) {
-				// TODO Auto-generated catch block
 				e.printStackTrace();
+			}
+
 		}
 		return groupList;
 	}
