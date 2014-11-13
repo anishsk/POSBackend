cbd231566a97856f3eeeddc4e6512f6626f3b524
Anish <anishsk@users.noreply.github.com>
2014-11-13 16:46:05 +0530

    Added New package TestClasses Moved Test.java from DatabaseConnection to TestClasses

---
+++ src/com/Hotelsoft/TestClasses/Test.java
@@ -0,0 +1,24 @@
+package com.Hotelsoft.TestClasses;
+
+import com.Hotelsoft.JavaClasses.JsonAggregator;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * GetDepartments men = new GetDepartments(); 
+		 * GetMenu items = new GetMenu(); 
+		 * GetGroups groups = new GetGroups();
+		 * men.returnDepartments(); 
+		 * items.returnMenuItems();
+		 * groups.returnGroupList();
+		 */
+		for (int i = 0; i < 5; i++) {
+			JsonAggregator j = new JsonAggregator();
+			j.getAllJson();
+		}
+	}
+
+}
