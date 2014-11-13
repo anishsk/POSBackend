dbc777f9699f6e5c197899883f3981c0b2200c2f
Anish <anishsk@users.noreply.github.com>
2014-11-13 22:08:58 +0530

    Added Properties File for db connection. Updated ConnectionManager to use this file.

--- src/com/Hotelsoft/DatabaseConnection/ConnectionManager.java
+++ src/com/Hotelsoft/DatabaseConnection/ConnectionManager.java
@@ -1,12 +1,18 @@
 package com.Hotelsoft.DatabaseConnection;
 
-/*import java.io.FileInputStream;
-import java.io.InputStream;*/
+/*
+ * import java.io.FileInputStream;
+ * import java.io.InputStream;
+ */
+import java.io.IOException;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 /*import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Properties;*/
+
+
+import java.util.Properties;
 
 import com.mysql.jdbc.Connection;
 /*import com.mysql.jdbc.ResultSet;
@@ -15,23 +21,33 @@ import com.mysql.jdbc.Statement;*/
 public class ConnectionManager {
 
 	public Connection connection = null;
-	static String dbuser = null;
-	static String dbpassword = null;
+	private static String dbUser = null;
+	private static String dbPassword = null;
+	private static String dbURL = null;
+	private static String dbDriver = null;
+	
 
 	public Connection getConnection() {
 		/*
 		 * Function to create connection to database 
 		 */
+		Properties prop = new Properties();
 		try {
-			Class.forName("com.mysql.jdbc.Driver").newInstance();
+			prop.load(Thread.currentThread().getContextClassLoader().getResourceAsStream("config.properties"));
+			dbUser = prop.getProperty("jdbc.username");
+			dbPassword = prop.getProperty("jdbc.password");
+			dbURL = prop.getProperty("jdbc.url");
+			dbDriver = prop.getProperty("jdbc.driver");
+			Class.forName(dbDriver).newInstance();
+			//Class.forName("com.mysql.jdbc.Driver").newInstance();
 		} catch (InstantiationException | IllegalAccessException
-				| ClassNotFoundException e) {
+				| ClassNotFoundException | IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		try {
-			connection = (Connection) DriverManager.getConnection(
-					"jdbc:mysql://127.0.0.1:3306/alive", "root", "anishkumar");
+			connection = (Connection) DriverManager.getConnection(dbURL, dbUser, dbPassword);
+			//connection = (Connection) DriverManager.getConnection("jdbc:mysql://127.0.0.1:3306/alive", "root", "anishkumar");
 
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
