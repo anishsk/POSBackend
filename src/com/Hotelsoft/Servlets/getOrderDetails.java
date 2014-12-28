a3a3c83727b8e177a2974d525f4cf24f1fbc6945
Anish <anishsk@users.noreply.github.com>
2014-12-28 23:53:20 +0530

    added service for fetching order details.

--- src/com/Hotelsoft/Servlets/getOrderDetails.java
+++ src/com/Hotelsoft/Servlets/getOrderDetails.java
@@ -1,11 +1,14 @@
 package com.Hotelsoft.Servlets;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.Hotelsoft.JavaClasses.orderParser;
 
 /**
  * Servlet implementation class getOrderDetails
@@ -34,6 +37,10 @@ public class getOrderDetails extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		String orderString;
+		orderString=request.getParameter("");
+		orderParser op = new orderParser(orderString);
+		op.orderJsonParser();
 	}
 
 }
