beff98f439a9093ca9dfa968a4fa755b3e9febe0
Anish <anishsk@users.noreply.github.com>
2015-01-01 16:40:21 +0530

    Added KotGenerator which may or maynot be used to retrive the and modify KOT

---
+++ src/com/Hotelsoft/JavaClasses/KotGenerator.java
@@ -0,0 +1,13 @@
+package com.Hotelsoft.JavaClasses;
+
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+import org.joda.time.format.DateTimeFormat;
+import org.joda.time.format.DateTimeFormatter;
+
+import com.mysql.jdbc.ResultSetImpl;
+import com.mysql.jdbc.Statement;
+
+public class KotGenerator {
+
+}
